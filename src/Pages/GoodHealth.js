be7c10cdd9e5ae7ea035554cc535e90516c3c858
lakheraakshay <akshay.lakhera@easystepin.com>
2022-08-19 15:35:41 +0530

    json files

--- src/Pages/GoodHealth.js
+++ src/Pages/GoodHealth.js
@@ -1,13 +1,18 @@
 import React from "react";
+import Healthy from "../Healthy.json";
+import Unhealthy from "../Unhealthy.json";
 
 function GoodHealth() {
   const healthObject = JSON.stringify({ status: "healthy" });
   const healthObject2 = JSON.stringify({ status: "unhealthy" });
+
+  console.log(Healthy);
+
   return (
     <div>
-      Json 1<div>{healthObject}</div>
+      Json 1<div>{JSON.stringify(Healthy)}</div>
       <div style={{ marginTop: "20px" }}>
-        Json2 <div>{healthObject2}</div>
+        Json2 <div>{JSON.stringify(Unhealthy)}</div>
       </div>
     </div>
   );
